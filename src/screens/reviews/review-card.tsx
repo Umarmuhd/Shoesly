00c79525866dff28e5e047153c2bf0e6462da19f
Umar Muhammad <umarfarouqft@gmail.com>
2023-09-21 07:38:06 +0100

    feat: product reviews

--- src/screens/reviews/review-card.tsx
+++ src/screens/reviews/review-card.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Star from '@/images/star.svg';
 import { Image, Text, View } from '@/ui';
 
 export function ProductReviewCard({
@@ -13,11 +14,24 @@ export function ProductReviewCard({
   };
 }) {
   return (
-    <View className="flex w-full flex-row">
+    <View className="mb-4 flex w-full flex-row">
       <View>
-        <Image source={user.avatar} />
+        <Image source={user.avatar} alt="..." />
       </View>
-      <View>
+      <View className="ml-4">
+        <View className="mb-[5px] flex w-full flex-row justify-between">
+          <Text variant="sm" weight="bold">
+            {user.name}
+          </Text>
+          <Text variant="xs">Today</Text>
+        </View>
+        <View className="mb-2.5 flex flex-row items-center space-x-[5px]">
+          <Star width={12} height={12} fill={'#FCD240'} />
+          <Star width={12} height={12} fill={'#FCD240'} />
+          <Star width={12} height={12} fill={'#FCD240'} />
+          <Star width={12} height={12} fill={'#FCD240'} />
+          <Star width={12} height={12} fill={'#FCD240'} />
+        </View>
         <Text variant="sm" className="text-dark">
           {content}
         </Text>
