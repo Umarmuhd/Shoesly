92d9c7be107481529402f11c4a1a157f5c380808
Umar Muhammad <umarfarouqft@gmail.com>
2023-09-22 22:44:25 +0100

    updated the firebase function for review and ratings counting

--- functions/index.ts
+++ functions/index.ts
@@ -1,13 +1,22 @@
-const functions = require('firebase-functions');
+import * as admin from 'firebase-admin';
+import type * as firestore from 'firebase-admin/firestore';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp();
 
 exports.updateProductAverageRating = functions.firestore
   .document('products/{productId}')
-  .onWrite(async (change, context) => {
+  .onWrite(async (change, _context) => {
     // Get the product document.
     const productDocument = await change.after.ref.get();
 
     // Get the reviews array from the product document.
-    const reviews = productDocument.data().reviews;
+    const reviews: firestore.DocumentData[] = productDocument.data()?.reviews;
+
+    if (!reviews) {
+      console.log('No reviews found for product');
+      return null;
+    }
 
     // Calculate the average rating.
     let averageRating = 0;
@@ -18,7 +27,7 @@ exports.updateProductAverageRating = functions.firestore
 
     // Update the product average rating and number of ratings.
     await productDocument.ref.update({
-      averageRating,
-      numberOfRatings: reviews.length,
+      avg_rating: averageRating,
+      num_of_reviews: reviews.length,
     });
   });
